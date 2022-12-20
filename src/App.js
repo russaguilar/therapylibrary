e11b28b7be1047c93d7048049f475b5a5d724015
Russel Aguilar <43275322+russaguilar@users.noreply.github.com>
2022-12-21 05:35:56 +0800

    Added sidebar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/Home';
 import Exercise from './pages/exercise/Exercise';
 import Navbar from './components/Navbar';
 import About from './pages/about/About';
+import Sidebar from './components/Sidebar';
 
 
 function App() {
@@ -12,12 +13,20 @@ function App() {
       <BrowserRouter>
 
       <Navbar />
-
+    <div className='container'>  
+    <div className='main'>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element ={<About />} />
+        <Route path='/about' element={<About />} />
         <Route path='/exercise/:id' element={<Exercise />} />
       </Routes>
+    </div>
+    <div className='sidebar'>
+      <Routes>
+        <Route path='/sidebar' element={<Sidebar />} />
+      </Routes>
+    </div>
+    </div>  
       </BrowserRouter>
       
     </div>
